0ee791fc1f750f390fc12433e0fe05b6eb07b946
Dhia-chan <ardhia>
2016-03-02 14:42:32 +0700

    my first

---
+++ resources/views/ukhti.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="assets/js/jquery-1.9.0.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.nivo.slider.js"></script>
+	<link href="assets/js/nivo-slider.css" rel="stylesheet" type="text/css" />
+	<link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="assets/js/default.css" rel="stylesheet" type="text/css" />
+	<title> Ukhti </title>
+	<script type="text/javascript">
+			$(window).load(function() {
+				$('#slider').nivoSlider();
+			});
+	</script>
+    <link rel="stylesheet" href="assets/images/themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/light/light.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/dark/dark.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/bar/bar.css" type="text/css" media="screen" />
+	<link href='https://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
+	<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
+</head>
+<body>
+	<div class="all-wrapper">
+		<div id="header">
+			<div class="log">
+				<a href="index.php"><div id="logo">Ukhti</div></a>
+				<div id="log">
+					<ul>
+						<li><a href="about.php">About</a></li>
+						<li><a href="logUp.php">Log Up</a></li>
+						<li><a href="logIn.php">Log In</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="nav">
+				<ul>
+					<div class="nav-in"><li><a href="jubel.php">Toko</a></li></div>
+					<div class="nav-in"><li><a href="artikel.php">Artikel</a></li></div>
+					<div class="nav-in"><li><a href="tutorial.php">Tutorial</a></li></div>
+					<div class="nav-in"><li><a href="hadits.php">Hadits</a></li></div>
+				</ul>
+			</div>
+		</div>
+		<div id="bar">
+			<div class="search">
+				<form>
+					<input type="text" name="input-search" placeholder="Search" class="text"></input>
+					<input type="submit" name="search" value="" class="submit"></input>
+				</form>
+			</div>
+			<div class="_breadcrumb">
+				<!-- another version - flat style with animated hover effect -->
+				<div class="breadcrumb flat">
+					<a href="#" class="active">Home</a>
+				</div>
+			</div>
+		</div>
+		<div id="wrapper">
+			<div class="wrap">
+				<div class="content">
+					<div id="js">
+						<div class="slider-wrapper theme-default">
+							<div id="slider" class="nivoSlider">
+								<img src="assets/images/1.jpg">
+								<img src="assets/images/2.jpg">
+								<img src="assets/images/3.jpg">
+								<img src="assets/images/4.jpg">
+								<img src="assets/images/5.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="rightKet">
+					<div id="arsip">
+						<div class="judul">Arsip</div>
+						<div class="isi-arsip">
+						        <ul>
+						            <li>
+						  				<a href="#arsip2016">2016</a>
+						  				<ul class="sidebar">
+										    <li><a href="#januari">Januari</a></li>
+										    <li><a href="#febuari">Febuari</a></li>
+										    <li><a href="#maret">Maret</a></li>
+										    <li><a href="#apri">Apri</a></li>
+										    <li><a href="#mei">Mei</a></li>
+										    <li><a href="#juni">Juni</a></li>
+										    <li><a href="#juli">Juli</a></li>
+										    <li><a href="#agustus">Agustus</a></li>
+										    <li><a href="#september">September</a></li>
+										    <li><a href="#oktober">Oktober</a></li>
+										    <li><a href="#november">November</a></li>
+										    <li><a href="#desember">Desember</a></li>
+										  </ul>
+									</li>
+						            <li>
+						  				<a href="#arsip2015">2015</a>
+						  				<ul class="sidebar">
+										    <li><a href="#januari">Januari</a></li>
+										    <li><a href="#febuari">Febuari</a></li>
+										    <li><a href="#maret">Maret</a></li>
+										    <li><a href="#apri">Apri</a></li>
+										    <li><a href="#mei">Mei</a></li>
+										    <li><a href="#juni">Juni</a></li>
+										    <li><a href="#juli">Juli</a></li>
+										    <li><a href="#agustus">Agustus</a></li>
+										    <li><a href="#september">September</a></li>
+										    <li><a href="#oktober">Oktober</a></li>
+										    <li><a href="#november">November</a></li>
+										    <li><a href="#desember">Desember</a></li>
+										  </ul>
+									</li>
+						            <li>
+						  				<a href="#arsip2014">2014</a>
+						  				<ul class="sidebar">
+										    <li><a href="#januari">Januari</a></li>
+										    <li><a href="#febuari">Febuari</a></li>
+										    <li><a href="#maret">Maret</a></li>
+										    <li><a href="#apri">Apri</a></li>
+										    <li><a href="#mei">Mei</a></li>
+										    <li><a href="#juni">Juni</a></li>
+										    <li><a href="#juli">Juli</a></li>
+										    <li><a href="#agustus">Agustus</a></li>
+										    <li><a href="#september">September</a></li>
+										    <li><a href="#oktober">Oktober</a></li>
+										    <li><a href="#november">November</a></li>
+										    <li><a href="#desember">Desember</a></li>
+										  </ul>
+									</li>
+						        </ul>
+						    </nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="logoF"><a href="index.php"> Ukhti </a></div>
+			<div class="info">
+				<div id="kontak">
+					<a href="https://www.facebook.com/ukhtiwebsiteinformation/?ref=aymt_homepage_panel"><div id="icon-fb"></div></a>
+					<a href=""><div id="icon-twitt"></div></a>
+					<div id="icon-g"></div>
+					<div id="icon-kontak"></div>
+				</div>
+				<div id="subscribe">
+					<div class="judul-subscribe">Berlangganan Artikel di Ukhti Yuu..</div>
+					<form class="form-subscribe">
+						<input name="mail" placeholder="Your E-mail Address" class="input-subscribe" type="email">
+						<input name="subscribe" value="Subscribe Now!" class="submit-subscribe" type="submit">
+					</form>
+				</div>
+			</div>
+			<div class="info1">
+				<div id="judul">Partner</div>
+				<div id="out-logo">
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+				</div>
+			</div>
+			<div class="info">
+				<div id="judul">About Ukhti</div>
+				<div id="aboutringkasan">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</div>
+			<div class="copyright">Ukhti &copy 2016</div>
+		</div>
+	</div>
+</body>
+</html>
