53f78c41ff2206b6d61bf4ab84529a027315c5f5
Dhia-chan <ardhia>
2016-03-07 11:24:34 +0700

    Pages Controller Added!

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,34 +21,6 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
 
-        Schema::create('signUp', function (Blueprint $table) {
-            $table->increments('id');
-            $table->text('nama_lengkap');
-            $table->string('username');
-            $table->text('tempat_lahir');
-            $table->date('tanggal_lahir');
-            $table->text('jenis_kelamin');
-            $table->string('status');
-            $table->string('email');
-            $table->string('al_email');
-            $table->string('password');
-        });
-
-        Schema::create('signIn', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email');
-            $table->string('password');
-        });
-
-        Schema::create('admin', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username');
-            $table->string('email');
-            $table->string('al_email');
-            $table->string('password');
-        });
-    }
-
     /**
      * Reverse the migrations.
      *
