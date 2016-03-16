0e6e74df7f76bf6d69f34c4e0e81f754cd1624a3
Shania14 <shania.indriani14@gmail.com>
2016-03-16 10:37:00 +0700

    :)

--- database/migrations/2016_03_16_032916_create_tutorial_table.php
+++ database/migrations/2016_03_16_032916_create_tutorial_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImagesTable extends Migration
+class CreateTutorialTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::table('images', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('description');
-            $table->string('filePath');
+        Schema::create('tutorial', function(Blueprint $table) {
+            $table->increments('No');
+            $table->text('Judul_Tutorial');
+            $table->text('Isi_Tutorial');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('images');
+        //
     }
 }
--- database/migrations/2016_03_16_033219_create_artikel_table.php
+++ database/migrations/2016_03_16_033219_create_artikel_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTutorialTable extends Migration
+class CreateArtikelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,6 +12,12 @@ class CreateTutorialTable extends Migration
      */
     public function up()
     {
+        Schema::create('artikel', function(Blueprint $table) {
+            $table->increments('No');
+            $table->text('Judul_Artikel');
+            $table->text('Isi_Artikel');
+            $table->timestamps();
+        });
     }
 
     /**
