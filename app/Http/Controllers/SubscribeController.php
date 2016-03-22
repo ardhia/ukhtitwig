2c0943ba63cd8b48e9b7072affacacfffaa2cf25
rina yulyani <rinayulyani12@gmail.com>
2016-03-22 12:29:05 +0700

    puluu

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Validator;
+use App\Subscribe;
 use App\Http\Requests;
 
 class SubscribeController extends Controller
 {
-	public function simpanLangganan (Requests $request){
+    
+	protected $redirectTo = '/';
+	protected $redirectPath = '/subs';
+	protected $subscribePath = '/';
 
-		$this->validate($request, [
-			'Email' => 'required|unique:posts|max:255',
-		]);
+	protected function simpanLangganan(array $request)
+    {
+    	 return Validator::make($request, [
+            'email' => 'required|email|unique:subscribe|max:255',
+        ]);
 
-	}
+    }
+
+  protected function create(array $request)
+  {
+
+  	return Subscribe::create([
+  		'email' => $request['email'],
+  		]);
+  }
 }
