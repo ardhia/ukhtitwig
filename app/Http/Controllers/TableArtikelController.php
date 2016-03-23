4ac4ef08d15dae4036115c317dd78ee35d3f7d5f
rina yulyani <rinayulyani12@gmail.com>
2016-03-23 13:52:26 +0700

    table artikel

---
+++ app/Http/Controllers/TableArtikelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+
+class TableArtikelController extends Controller
+{
+    
+     public function tampilTableArtikel () {
+    	$daftarartikel =  DB::table('artikel')->select('No', 'Judul_Artikel', 'Isi_Artikel')->get();
+
+    	return view('admin/tableArtikel', ['artikel' => $daftarartikel]);
+    }
+
+
+}
