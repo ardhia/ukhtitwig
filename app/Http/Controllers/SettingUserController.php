d63318bb02fa0f2f8390770046630dd4600efcf3
rina yulyani <rinayulyani12@gmail.com>
2016-04-08 15:01:00 +0700

    setting

--- app/Http/Controllers/SettingUserController.php
+++ app/Http/Controllers/SettingUserController.php
@@ -18,8 +18,7 @@ class SettingUserController extends Controller
 	}
 
 	public function prosesEditSetting (Request $request, $id) {
-    $user = DB::table('users')
-            ->where('id', $id)
+    $user = User::where('id', $id)
             ->update(['name' => $request->input('name'), 'email' => $request->input('email'), 'password' => $request->input('password'), 'tempat_lahir' => $request->input('tempat_lahir'), 'tanggal_lahir' => $request->input('tanggal_lahir'), 'jenis_kelamin' => $request->input('jenis_kelamin'), 'status' => $request->input('status'), 'al_email' => $request->input('al_email'), 'alamat' => $request->input('alamat'), 'noHP' => $request->input('noHP'), 'kesukaan' => $request->input('kesukaan'), 'kutipanHadits' => $request->input('kutipanHadits'), 'statusUser' => $request->input('statusUser'), 'contactUs' => $request->input('contactUs')]);
 
         //dd($user);exit;
