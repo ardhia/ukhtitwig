20e494c6d69536562cd155d4a2eda8bc89ac9d45
Shania14 <shania.indriani14@gmail.com>
2016-04-11 18:20:44 +0700

    :)

--- app/Http/Controllers/HelpsignupController.php
+++ app/Http/Controllers/HelpsignupController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests;
+use App\Notifikasi;
 
 class HelpsignupController extends Controller
 {
