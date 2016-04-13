32fb97a77c79e9ca5ad6e6e3686bee841c4cfd6a
Shania14 <shania.indriani14@gmail.com>
2016-04-13 13:09:00 +0700

    Allhamdulillah :')

--- database/migrations/2016_04_11_070025_update_hadits_table.php
+++ database/migrations/2016_04_11_070025_update_hadits_table.php
@@ -12,14 +12,9 @@ class UpdateHaditsTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2016_04_08_031350_update_tutorial_table.php
-        Schema::table('tutorial', function ($table) {
-            $table->string('konfirmasi');
-            $table->string('user_name');
-=======
         Schema::table('hadits', function ($table) {
             $table->string('judul');
->>>>>>> ced5837c5b04cd690ff7143be05572b7fafc0906:database/migrations/2016_04_11_070025_update_hadits_table.php
+
         });
     }
 
