5f1b2f04f541b143c04ff9b672cb6c977c570067
Dhia-chan <ardhia>
2016-04-20 09:27:44 +0700

    done!

--- database/migrations/2016_04_04_103422_create_komentar_tutorial_table.php
+++ database/migrations/2016_04_04_103422_create_komentar_tutorial_table.php
@@ -20,6 +20,7 @@ class CreateKomentarTutorialTable extends Migration
             $table->foreign('no_tutorial')->references('No')->on('tutorial');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->text('Judul');
             $table->timestamps();
         });
     }
--- database/migrations/2016_04_07_043411_create_komentarArtikel_table.php
+++ database/migrations/2016_04_07_043411_create_komentarArtikel_table.php
@@ -20,6 +20,7 @@ class CreateKomentarArtikelTable extends Migration
             $table->foreign('no_artikel')->references('No')->on('artikel');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->text('Judul');
             $table->timestamps();
         });
     }
